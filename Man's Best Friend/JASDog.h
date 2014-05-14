e5a3aea6210fbef5b862c842d554591642fa3999
Joshua Sargent <Sargent@Joshuas-MacBook-Pro.local>
2014-05-14 11:14:16 -0600

    first commit

---
+++ Man's Best Friend/JASDog.h
@@ -0,0 +1,13 @@
+//
+//  JASDog.h
+//  Man's Best Friend
+//
+//  Created by Joshua Sargent on 5/11/14.
+//  Copyright (c) 2014 DreamLiveCapture. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JASDog : NSObject
+
+@end
---
+++ Man's Best Friend/JASDog.m
@@ -0,0 +1,13 @@
+//
+//  JASDog.m
+//  Man's Best Friend
+//
+//  Created by Joshua Sargent on 5/11/14.
+//  Copyright (c) 2014 DreamLiveCapture. All rights reserved.
+//
+
+#import "JASDog.h"
+
+@implementation JASDog
+
+@end
